ed3ac7a3a6a7f516dad1782a5ca714e6d365bbc2
Daniel <64807656+DanielNinchev@users.noreply.github.com>
2021-09-12 21:31:41 +0300

    Added more elements
    Added:
    - fire ammo bonus;
    - fire speed bonus;
    - fire power bonus;
    - speed bonus;
    - day element;
    - night element;
    - death element;

--- gameFactory.js
+++ gameFactory.js
@@ -1,7 +1,7 @@
 function gameFactory() {
     let getState = gameStateFactory();
 
-    let { wizard, bugStats, fireBallStats, lifeStats } = getState();
+    let { wizard, bugStats, fireBallStats, lifeStats, firePowerStats, fireAmmoStats, fireSpeedStats, deathStats, dayStats, nightStats, speedBonusStats } = getState();
 
     let startScreen = document.querySelector('.start-screen');
     let scoreScreen = document.querySelector('.score-screen');
@@ -50,7 +50,84 @@ function gameFactory() {
             lifeBonusElement.style.top = (playScreen.offsetHeight - lifeStats.height) * Math.random() + 'px';
 
             playScreen.appendChild(lifeBonusElement);
-        }
+        },
+        createSpeedBonus: () => {
+            let speedBonusElement = document.createElement('div');
+
+            speedBonusElement.classList.add('speedBonus');
+            speedBonusElement.style.width = speedBonusStats.width + 'px';
+            speedBonusElement.style.height = speedBonusStats.height + 'px';
+            speedBonusElement.style.left = playScreen.offsetWidth - speedBonusStats.width + 'px';
+            speedBonusElement.style.top = (playScreen.offsetHeight - speedBonusStats.height) * Math.random() + 'px';
+
+            playScreen.appendChild(speedBonusElement);
+        },
+        createFirePowerBonus: () => {
+            let firePowerBonus = document.createElement('div');
+
+            firePowerBonus.classList.add('firePower');
+            firePowerBonus.style.width = firePowerStats.width + 'px';
+            firePowerBonus.style.height = firePowerStats.height + 'px';
+            firePowerBonus.style.left = playScreen.offsetWidth - firePowerStats.width + 'px';
+            firePowerBonus.style.top = (playScreen.offsetHeight - firePowerStats.height) * Math.random() + 'px';
+
+            playScreen.appendChild(firePowerBonus);
+        },
+        createFireAmmoBonus: () => {
+            let fireAmmoBonus = document.createElement('div');
+
+            fireAmmoBonus.classList.add('fireAmmo');
+            fireAmmoBonus.style.width = fireAmmoStats.width + 'px';
+            fireAmmoBonus.style.height = fireAmmoStats.height + 'px';
+            fireAmmoBonus.style.left = playScreen.offsetWidth - fireAmmoStats.width + 'px';
+            fireAmmoBonus.style.top = (playScreen.offsetHeight - fireAmmoStats.height) * Math.random() + 'px';
+
+            playScreen.appendChild(fireAmmoBonus);
+        },
+        createFireSpeedBonus: () => {
+            let fireSpeedBonus = document.createElement('div');
+
+            fireSpeedBonus.classList.add('fireSpeed');
+            fireSpeedBonus.style.width = fireSpeedStats.width + 'px';
+            fireSpeedBonus.style.height = fireSpeedStats.height + 'px';
+            fireSpeedBonus.style.left = playScreen.offsetWidth - fireSpeedStats.width + 'px';
+            fireSpeedBonus.style.top = (playScreen.offsetHeight - fireSpeedStats.height) * Math.random() + 'px';
+
+            playScreen.appendChild(fireSpeedBonus);
+        },
+        createDayBonus: () => {
+            let dayElement = document.createElement('div');
+
+            dayElement.classList.add('day');
+            dayElement.style.width = dayStats.width + 'px';
+            dayElement.style.height = dayStats.height + 'px';
+            dayElement.style.left = playScreen.offsetWidth - dayStats.width + 'px';
+            dayElement.style.top = (playScreen.offsetHeight - dayStats.height) * Math.random() + 'px';
+
+            playScreen.appendChild(dayElement);
+        },
+        createNightBonus: () => {
+            let nightElement = document.createElement('div');
+
+            nightElement.classList.add('night');
+            nightElement.style.width = nightStats.width + 'px';
+            nightElement.style.height = nightStats.height + 'px';
+            nightElement.style.left = playScreen.offsetWidth - nightStats.width + 'px';
+            nightElement.style.top = (playScreen.offsetHeight - nightStats.height) * Math.random() + 'px';
+
+            playScreen.appendChild(nightElement);
+        },
+        createDeathElement: () => {
+            let deathElement = document.createElement('div');
+
+            deathElement.classList.add('death');
+            deathElement.style.width = deathStats.width + 'px';
+            deathElement.style.height = deathStats.height + 'px';
+            deathElement.style.left = playScreen.offsetWidth - deathStats.width + 'px';
+            deathElement.style.top = (playScreen.offsetHeight - deathStats.height) * Math.random() + 'px';
+
+            playScreen.appendChild(deathElement);
+        },
     };
 
     return factory;
